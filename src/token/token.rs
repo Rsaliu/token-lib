25ed65f7f838fa01e5ad4be5dea1f49bd833e212
RSaliu <ridwanriley@gmail.com>
2024-07-03 00:55:28 +0100

    Update token class

--- src/token/token.rs
+++ src/token/token.rs
@@ -14,12 +14,21 @@ pub enum TokenType{
     RefreshToken,
     ActivationToken
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub token_uuid: Uuid,
+    pub exp: NaiveDateTime,
+    pub iat: NaiveDateTime,
+    pub nbf: NaiveDateTime,
+}
+
+
 #[derive(Debug, Deserialize, Serialize, Clone,Default)]
 pub struct Token{
     id: Uuid,
     token: String,
-    user_id: Uuid,
-    expired_in: NaiveDateTime,
     token_type: TokenType,
     blacklisted: bool
 }
@@ -27,23 +36,17 @@ pub struct Token{
 
 
 impl Token{
-    pub fn new(user_id:Uuid,token: String,expired_in: NaiveDateTime,token_type:TokenType)->Self{
-        Token {id:Uuid::nil(),token,user_id,expired_in,token_type,blacklisted:false}
+    pub fn new(token: String,token_type: TokenType)->Self{
+        Token {id:Uuid::nil(),token,token_type,blacklisted:false}
     }
-    pub fn new_full(id: Uuid,token: String,user_id: Uuid,expired_in: NaiveDateTime,token_type:TokenType,blacklisted:bool)->Self{
-        Token {id,token,user_id,expired_in,token_type,blacklisted}
+    pub fn new_full(id: Uuid,token: String,token_type: TokenType,blacklisted:bool)->Self{
+        Token {id,token,token_type,blacklisted}
     }
     pub fn get_token(&self)->&str{
         self.token.as_str()
     }
     pub fn get_id(&self)->Uuid{
         self.id
-    }
-    pub fn get_user_id(&self)->Uuid{
-        self.user_id
-    }
-    pub fn get_expired_time(&self)->NaiveDateTime{
-        self.expired_in
     }
     pub fn get_type(&self)->TokenType{
         self.token_type
